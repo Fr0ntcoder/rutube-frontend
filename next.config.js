fb05d1f0f31f5e3508d113e9d5dff3a8121c17e0
Fr0ntcoder <jambox228@gmail.com>
2022-11-29 13:15:49 +0800

    start settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL
+	},
+	images: {
+		domains: ['localhost']
+	},
+	sassOptions: {
+		includePaths: ['./src'],
+		prependData: `@import "@/assets/styles/scss/variables/index.scss";`
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:4200/api/:path*'
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'http://localhost:4200/uploads/:path*'
+			}
+		];
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
